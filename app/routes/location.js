c55461502a6a21fdfc366a4576c1e7907bb98f88
Alissa Paluska and Polina Nenchev <alissa.paluska@gmail.com>
2016-04-04 10:43:45 -0700

    Add delete button for location and create full functionality

--- app/routes/location.js
+++ app/routes/location.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('location', params.location_id);
+  },
+
+  actions: {
+    deleteLocation(model) {
+      var location_deletions = model.get('posts').map(function(post) {
+        return post.destroyRecord();
+      });
+      Ember.RSVP.all(location_deletions).then(function() {
+        return model.destroyRecord();
+      })
+      this.transitionTo('index');
+    }
+  }
 });
