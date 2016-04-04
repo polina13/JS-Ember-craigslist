41031ac42602249a2c53dc210b405523fb8a6636
Alissa Paluska and Polina Nenchev <polina.mihova@yahoo.com>
2016-04-04 15:24:26 -0700

    Add computed property fullPost

--- app/components/post-tile.js
+++ app/components/post-tile.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  fullPost: Ember.computed('post.author', 'post.category', function() {
+    return this.get('post.author') + ' | ' + this.get('post.category');
+  })
 });
--- app/routes/location.js
+++ app/routes/location.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('location', params.location_id);
   },
-
   actions: {
     deleteLocation(model) {
       var location_deletions = model.get('posts').map(function(post) {
