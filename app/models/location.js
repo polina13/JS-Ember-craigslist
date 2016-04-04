e41a2937d54161c99cc20da69ca11fc50ad12a4f
Alissa Paluska and Polina Nenchev <alissa.paluska@gmail.com>
2016-04-04 14:53:31 -0700

    Add computed property on location model

--- app/models/location.js
+++ app/models/location.js
@@ -4,5 +4,10 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   state: DS.attr('string'),
   country: DS.attr('string'),
-  posts: DS.hasMany('post', {async: true})
+  posts: DS.hasMany('post', {async: true}),
+  //example of computed property within model
+  fullLocation: Ember.computed('name', 'state', 'country', function() {
+    return this.get('name') + ', ' + this.get('state') + ' | ' + this.get('country');
+  })
 });
+//can be referenced on location.hbs, by accessing model hook, via model.fullLocation
